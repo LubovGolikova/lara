c6019577ae2b2a0760fa56ec110b3fab175213a5
eromasters <evromastersx@gmail.com>
2020-05-08 11:36:07 +0300

    new lending

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,67 +1,76 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="bd-example">
-        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="assets/photo5.jpg" class="d-block w-100"  alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Украсьте свою жизнь цветами!!!</h5>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="assets/photo6.jpg" class="d-block w-100"   alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Украсьте свою жизнь цветами!!!</h5>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="assets\photo7.jpg" class="d-block w-100"   alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Украсьте свою жизнь цветами!!!</h5>
-                    </div>
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
+<section class="box-welcome">
+    <div class="welcome">
+          <div class="text"><span>Украсьте</span> <span>свою жизнь</span> <span>цветами!!!</span></div>
+           <img src="{{asset('assets/images/box-1.png')}}" alt=""/>
     </div>
-
-    <div class="row mt-3">
-        <div class="col-md-8 order-2">
-            <div class="flowers px-5">
-                <div class="slider">
-                    @foreach($sliderFlowers as $flower)
-                    <div>
-                     <a href="/flower/{{$flower->id}}"><img src="{{$flower->getThumb()}}" class="img-fluid"></a>
-                    <div>
-                         <a href="/flower/{{$flower->id}}">{{$flower->name}}</a><br>
-                        {{$flower->stock?'в наличии':'нет в наличии'}}<br>
-                        {{$flower->price}}<br>
-                       <a href="/flower/{{$flower->id}}"> <button class="btn btn-success my-2 my-sm-0">Купить</button></a>
+</section>
+<section class="box-new-flowers">
+        <div class=" row mt-3">
+            <div class="col-md-6 order-2">
+                <div class="container-news">
+                    <div class="row justify-content-between mr-4 mb-4">
+                       <p>Новинки</p>
+                       <div class="figure">
+                            <img src="{{asset('assets/images/fig1.png')}}" alt=""/>
+                            <img src="{{asset('assets/images/fig2.png')}}" alt=""/>
+                            <img src="{{asset('assets/images/fig3.png')}}" alt=""/>
+                            <img src="{{asset('assets/images/fig4.png')}}" alt=""/>
+                        </div>
                     </div>
+                </div>
+                <div class="flowers px-5">
+                    <div class="slider">
+                        @foreach($sliderFlowers as $flower)
+                        <div>
+                            <div class="border-circle">
+                            <a href="/flower/{{$flower->id}}"><img src="{{$flower->getThumb()}}" class="img-fluid "></a>
+                            </div>
+                        </div>
+                        @endforeach
                     </div>
-                    @endforeach
                 </div>
             </div>
+             <div class="col-md-6 order-1">
+                <div class="about">
+                    <h3> О нас </h3>
+                  <p> Рада приветствовать всех кто заглянул на наш сайт! Это небольшой магазин лилейников. Лилейник-это
+                  цветок, который цветет один день, но когда куст разростается этого недостаткам вы не заметите,
+                  ведь он весь будет покрыт бутонами и вы будете в течение долгого времени созерцать прекрасное цветение!
+                  Здесь вы найдете лилейник на любой вкус! У нас представлены как зарегестрированные сорта
+                  так и сеянцы, которые были выведены гибридизаторами-любителями, но не были зарегестрированы,
+                   такие лилейники стоят намного дешевле, однако это никак не умоляет их красоты!
+                  Покупая здесь лилейники вы получите хороший посадочный материал! Сортность и качество гарантирую!
+                   Желаю вам приятных покупок!</p>
+                </div>
+             </div>
         </div>
-        <div class="col-md-4 order-1">
-            <div class="about">
-                <h3> О нас </h3>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aspernatur at beatae commodi debitis eligendi ex exercitationem expedita fugiat illo ipsam, possimus provident qui recusandae sequi sit voluptatibus. Consequuntur, deserunt.
+</section>
+<section class="box-news">
+            <div class="container-news">
+                <div class="row justify-content-between mr-4 mb-1">
+                   <p>Уход</p>
+                   <div class="figure">
+                        <img src="{{asset('assets/images/fig5.png')}}" alt=""/>
+                   </div>
+                </div>
             </div>
-        </div>
-    </div>
+            <div class="container-care">
+                <div class="gardening">
+                     <img src="{{asset('assets/images/garden1.jpg')}}" alt=""/>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+                    blanditiis delectus, deleniti deserunt dolores error explicabo facilis ipsa iusto
+                     labore laborum minima perferendis perspiciatis, quae quas quibusdam quos sed, similique?</p>
+                </div>
+                <div class="gardening">
+                     <img src="{{asset('assets/images/garden2.jpg')}}" alt=""/>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+                     blanditiis delectus, deleniti deserunt dolores error explicabo facilis
+                     ipsa iusto labore laborum minima perferendis perspiciatis, quae quas quibusdam
+                      quos sed, similique?</p>
+                 </div>
+            </div>
+ </section>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,50 +13,64 @@
     <link rel="stylesheet" href="{{asset('styles/style.css')}}">
 </head>
 <body>
-<div class="content">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-        <a class="navbar-brand" href="/">
-            Сад <br>Лилейников
-        </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mx-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">О нас <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Услуги</a>
-            </li>
-            <li class="nav-item ">
-                <a class="nav-link" href="#">Растения </a>
-            </li>
-            <li class="nav-item ">
-                <a class="nav-link" href="/shop">Магазин </a>
-            </li>
-            <li class="nav-item ">
-                <a class="nav-link" href="#">Контакты </a>
-            </li>
-        </ul>
-        <div class="social">
-            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+                <div class="navbar-brand "> <a href="/">
+                    <img src="{{asset('assets/images/logo.png')}}" alt=""/></a>
+                </div>
+             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                     <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto align-md-items-center">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">О нас <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Услуги</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="#">Контакты </a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="#">Растения </a>
+                    </li>
+                    <li class="nav-item dropdown ">
+                        <a class="nav-link dropbtn {!!Request::is('shop') ? 'active' : '' !!}"
+                        id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false" href="/shop">Магазин </a>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                <a class="dropdown-item" href="#">До 100 грн.</a>
+                                <a class="dropdown-item" href="#">От 100 грн. и более</a>
+                                <a class="dropdown-item" href="#">Все</a>
+                            </div>
+                    </li>
+                    <li class="nav-item">
+                       <div class="box">
+                           <a href="#" data-toggle="modal" data-target="#exampleModal">
+                             <img src="{{asset('assets/images/shopping-cart.png')}}" alt=""/>
+                           </a>
+                       </div>
+                    </li>
+                </ul>
+                <div class="social">
+                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </div>
+             </div>
         </div>
-        <div class="box">
-           <a href="#" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+     </nav>
+     <div class="container">
+        <div class="search-line py-3 fluid container d-flex flex-row justify-content-center align-items-center">
+             <form class="search form-inline col-12 col-lg-6  my-2 my-lg-0 p-0 " method="get" action="/search">
+                <input class="form-control mr-sm-2" name="s"  autocomplete="off" contenteditable="true" spellcheck="false"
+                 type="search" placeholder="Поиск" aria-label="Search">
+                <button class="btn btn-default my-2 my-sm-0" type="submit">Найти</button>
+             </form>
         </div>
     </div>
-    </div>
-</nav>
-    <div class="search-line mt-3 fluid container d-flex flex-row justify-content-center align-items-center">
-    <form class="form-inline my-2 my-lg-0 " method="get" action="/search">
-        <input class="form-control mr-sm-2" name="s"  autocomplete="off" contenteditable="true" spellcheck="false" type="search" placeholder="Поиск" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Найти</button>
-    </form>
-    </div>
-<div class="container base-color">
+<div class="container base-color mb-5">
     @yield('content')
 </div>
 
@@ -83,9 +97,8 @@
   </div>
 </div>
 
-</div>
 
-<footer class="footer-color d-flex flex-row justify-content-center align-items-center">
+<footer class="footer-color footer-light footer-nv d-flex flex-row justify-content-center align-items-center">
         <div class="col-3">
         <h3>О нас</h3>
         </div>
@@ -105,6 +118,8 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script type="text/javascript" src="{{asset('assets/slick/slick.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/script.js')}}"></script>
+
+
 </body>
 
 </html>
